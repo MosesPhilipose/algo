62ef09c77e641b1ba0cc1e0c886f7e32cc329a12
MosesPhilipose <mosesphilipose@gmail.com>
2025-03-13 14:17:42 +0530

    Update urls.py

--- my_nifty_project/my_nifty_project/urls.py
+++ my_nifty_project/my_nifty_project/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('indexstats.urls')),  # Include the indexstats app's URLs
+    path('', include('my_nifty_project.indexstats.urls')),
+    path('healthz', lambda request: HttpResponse("OK"), name='health_check'),  # For Render
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
