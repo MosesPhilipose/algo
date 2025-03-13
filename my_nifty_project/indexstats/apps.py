933cbad95e113c70b1ee5947b407126a78b48a49
MosesPhilipose <mosesphilipose@gmail.com>
2025-03-13 13:36:54 +0530

    Update apps.py

--- my_nifty_project/indexstats/apps.py
+++ my_nifty_project/indexstats/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class IndexstatsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "indexstats"
+    name = "my_nifty_project.indexstats"
